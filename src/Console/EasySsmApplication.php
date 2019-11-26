3715ab9762ab12ed301c1e402acf18c64fe8f220
natepage <nathan.page86@gmail.com>
2019-11-26 13:48:10 +1100

    Update app version

--- src/Console/EasySsmApplication.php
+++ src/Console/EasySsmApplication.php
@@ -11,7 +11,7 @@ final class EasySsmApplication extends Application
     use HelpfulApplicationTrait;
 
     /** @var string */
-    public const VERSION = '1.0.0';
+    public const VERSION = '1.0.1';
 
     /**
      * EasySsmApplication constructor.
